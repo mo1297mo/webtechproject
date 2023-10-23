fc9069b694ef56b544fc4fa9b14f89ff1a5fedf4
Ferdous Rahsepar <rahsepar19@gmail.com>
2023-10-23 15:43:28 +0200

    vue.js hello world

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -2,3 +2,15 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+
+// Path: vue.config.js
+module.exports = {
+  // ...
+  devServer: {
+    open: process.platform === 'darwin',
+    host: '0.0.0.0',
+    port: 8081, // CHANGE YOUR PORT HERE!
+    https: false,
+  },
+  // ...
+}
