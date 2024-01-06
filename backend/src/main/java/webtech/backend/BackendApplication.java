cf6ca1f6777b6e362a31a873c00d9ec61b3674ca
mojeeeb <s0588473@htw-berlin.de>
2024-01-06 17:29:34 +0100

    bug fix

--- backend/src/main/java/webtech/backend/BackendApplication.java
+++ backend/src/main/java/webtech/backend/BackendApplication.java
@@ -1,5 +1,7 @@
 package webtech.backend;
 
+import jakarta.annotation.PostConstruct;
+import org.springframework.beans.factory.annotation.Autowired;
 import webtech.backend.seed.*;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,10 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class BackendApplication {
 
+	@Autowired
+	private DatabaseSeeder seeder;
+
 	public static void main(String[] args) {
 		SpringApplication.run(BackendApplication.class, args);
-		DatabaseSeeder seeder = new DatabaseSeeder();
-        seeder.updateTablesAndAvailability();
+	}
+
+	@PostConstruct
+	public void init() {
+		seeder.updateTablesAndAvailability();
 	}
 
 }
