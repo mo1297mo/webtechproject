cfab54381e998be0e74cc424992b03fe6c3a7ec8
mojeeeb <s0588473@htw-berlin.de>
2024-01-14 16:33:36 +0100

    fix

--- frontend/src/router/router.js
+++ frontend/src/router/router.js
@@ -1,13 +1,19 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import ConfirmationPage from '@/components/ConfirmationPage.vue';
-import CancelReservation from '@/components/CancelReservation.vue'; // Import the CancelReservation component
+import CancelReservation from '@/components/CancelReservation.vue';
+import ReservationForm from '@/components/ReservationForm.vue';
 
 Vue.use(Router);
 
 export default new Router({
     mode: 'history',
     routes: [
+        {
+            path: '/',
+            name: 'reservationForm',
+            component: ReservationForm // Setzen Sie die ReservationForm als Startseite
+        },
         {
             path: '/confirmation',
             name: 'confirmation',
